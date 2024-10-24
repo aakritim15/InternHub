50bca755877c60c53d149dbb541278d5b092e39f
Aakriti Mehta <aakritimehta15@gmail.com>
2024-10-24 20:57:15 +0530

    recruiter images

--- client/recruiter/src/components/Footer.jsx
+++ client/recruiter/src/components/Footer.jsx
@@ -4,31 +4,47 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-section">
-          <h4>Job Portal</h4>
-          <p>Your go-to platform for finding the best jobs.</p>
-        </div>
-        <div className="footer-section">
-          <h4>Quick Links</h4>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about-us">About Us</a></li>
-            <li><a href="#contact-us">Contact Us</a></li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h4>Follow Us</h4>
-          <div className="social-icons">
-            <a href="#facebook">Facebook</a>
-            <a href="#twitter">Twitter</a>
-            <a href="#linkedin">LinkedIn</a>
+      <div className='footerHolder'></div>
+        <div class='footerContainer'>
+          <div class='ThelogoContainer'>
+              <div className="logo" style={{display: 'flex', flexDirection: 'row', gap: '2vh', fontWeight: 'bold', fontSize: '4vh', justifyItems: 'center', alignItems: 'center'}}>
+              <img src="/imgs/logoWhite.svg" alt="InternHub" style={{width: '6vh', height: '6vh'}} />
+              <div style={{color: '#ffffff'}}>InternHub</div>
+              </div>
+            <p>Your go-to platform for<br /> finding the best jobs.</p>
+            <ul>
+              <li><img src="/imgs/SocialMedia/Facebook.svg" /></li>
+              <li><img src="/imgs/SocialMedia/Instagram.svg" /></li>
+              <li><img src="/imgs/SocialMedia/LinkedIn.svg" /></li>
+              <li> <img src="/imgs/SocialMedia/Twitter.svg" /></li>
+              <li><img src="/imgs/SocialMedia/YouTube.svg" /></li>
+            </ul>
           </div>
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <p>&copy; 2024 Job Portal. All rights reserved.</p>
-      </div>
+          <div className="custFlex2">
+            <div>Contact Us</div>
+          <div class="footerContacts">
+            <p class="footerGreen"></p>
+            <span>
+              <span>
+                <img src="/imgs/Contacts/Email.svg" />
+              </span>
+              <span>internhub@somaiya.com</span>
+            </span>
+            <span>
+              <span>
+                <img src="/imgs/Contacts/Phone.svg" />
+              </span>
+              <span>+91 90876 54321</span>
+            </span>
+            <span>
+              <span>
+                <img src="/imgs/Contacts/Mark.svg" />
+              </span>
+              <span>KJSCE, Somaiya Vidyavihaar University, Mumbai, 400067</span>
+            </span>
+          </div>
+          </div>
+          </div>
     </footer>
   );
 };
